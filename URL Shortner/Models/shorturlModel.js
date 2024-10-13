8497e881eb1df06ff5f0e5dc90644a061c7ca314
akashjack <akashjack.ar57@gmail.com>
2024-10-13 08:25:30 +0530

    url shortner with JSON

--- URL Shortner/Models/shorturlModel.js
+++
@@ -1,9 +0,0 @@
-// models/shorturlModel.js
-const mongoose = require('mongoose');
-
-const shorturlSchema = new mongoose.Schema({
-  short_url: { type: Number, required: true, unique: true },
-  original_url: { type: String, required: true },
-});
-
-module.exports = mongoose.model('ShortURL', shorturlSchema);
